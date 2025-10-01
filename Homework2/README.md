d3bbe1924dca54749897a4daa57fb723b67ae222
aws user <aws.user12358@gmail.com>
2025-10-01 12:51:23 +0300

    Homework2 Docker/Docker COmpose/LLM

---
+++ Homework2/README.md
@@ -0,0 +1,30 @@
+### Homework 2
+#### Introduction to Docker and Docker Compose
+
+Task 4  - Run same application using Docker container
+![docker build](./img/docker-build.PNG)
+
+![Docker Images](./img/docker-image.PNG)
+
+![Docker Container](./img/docker-container.PNG)
+
+
+Task 5 - Visualize another file with Sample application (use bind mounts & endpoint & cmd combination)
+![Docker Another File](./img/docker-run-another-file.PNG)
+
+![Docker endpoint cmd](./img/docker-endpoint-cmd.png)
+
+![Docker Application](./img/docker-app.PNG)
+
+
+
+Task 6 - Attach volume to the container
+![Docker Attach Volume](./img/docker-attach-volume.PNG)
+
+
+Task 7 - Add network to the containers
+![Docker Network](./img/docker-network.png)
+
+
+Task 8 - Advanced Task - Run LLM chatbot application using Docker Compose
+![Docker Compose chatbot](./img/docker-chatbot.png)
---
+++ Homework2/llm-chatbot/web/app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import requests
+import random
+import time
+
+
+
+url = "http://model-runner.docker.internal:12434/engines/llama.cpp/v1/chat/completions"
+
+headers = {"Content-Type": "application/json"}
+
+st.write("Streamlit loves LLMs! 🤖 [Build your own chat app](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps) in minutes, then make it powerful by adding images, dataframes, or even input widgets to the chat.")
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Let's start chatting! 👇"}]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        data = {
+            "model": "ai/smollm2:135M-Q4_0",
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+        }
+
+        assistant_response = requests.post(url, headers=headers, json=data).json()["choices"][0]['message']['content']
+        full_response = ""
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
